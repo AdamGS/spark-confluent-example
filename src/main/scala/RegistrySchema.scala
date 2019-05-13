5ab52da34f4d3d464c95b4cf3e9a8ac888758e22
Adam Gutglick <adamgsal@gmail.com>
2019-05-13 15:37:47 +0300

    Remove redundent case class

--- src/main/scala/RegistrySchema.scala
+++
@@ -1,7 +0,0 @@
-import spray.json.DefaultJsonProtocol
-
-case class RegistrySchema(subject:String, version: Int, id: Int, schema: String)
-
-object RegistrySchemaProtocol extends DefaultJsonProtocol {
-  implicit val schemaFormt = jsonFormat4(RegistrySchema)
-}
